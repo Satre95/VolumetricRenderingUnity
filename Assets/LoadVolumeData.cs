441cfab7e623206916e6e8ccfc7cf8356d92e297
Siddharth Atre <siddharth_atre@intuit.com>
2016-08-13 16:06:17 -0700

    Successfully imported images into 3d texture

--- Assets/LoadVolumeData.cs
+++ Assets/LoadVolumeData.cs
@@ -6,26 +6,41 @@ using System.IO;
 public class LoadVolumeData : MonoBehaviour {
 
 	public Texture3D volumeData;
-	private Texture2D[] images;
+	public List<Color> imageColors;
 
 	private const int numImages = 120;
 	// Use this for initialization
 	void Start () {
-
-
-
-
-		images = new Texture2D[numImages];
 		string head = "head-";
 		for (int i = 0; i < numImages; i++) {
 			Texture2D anImage = Resources.Load("head-images/" + head + i.ToString().PadLeft(3, '0')) as Texture2D;
-			print (anImage.GetPixel(128, 128));
-			images [i] = anImage;
+			addImageColorToList (anImage);
 		}
 
+		Color[] allColors = imageColors.ToArray ();
 
+		int texSize = 256 * 256 * 128;
+
+		Color[] allColorsWithPadding = new Color[texSize];
+
+		allColors.CopyTo (allColorsWithPadding, 0);
+		for (int i = allColors.Length; i < allColorsWithPadding.Length; i++) {
+			allColorsWithPadding [i] = new Color (0, 0, 0, 0);
+		}
+
+		volumeData = new Texture3D (256, 256, 128, TextureFormat.ARGB32, false);
+
+		volumeData.SetPixels (allColorsWithPadding);
+		volumeData.Apply ();
+		GetComponent<Renderer>().material.SetTexture("_Texture", volumeData);
 	}
-	
+
+	void addImageColorToList(Texture2D anImage){
+		Color[] tempColors = anImage.GetPixels ();
+		for (int i = 0; i < tempColors.Length; i++) {
+			imageColors.Add (tempColors [i]);
+		}
+	}
 	// Update is called once per frame
 	void Update () {
 	
